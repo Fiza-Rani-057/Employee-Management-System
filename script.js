155ba802fb58c7e2856256c0bd52ca26ba9e1d10
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-08-26 22:37:04 +0500

    Employee Managment System

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
- let email = document.getElementById('email');
- let password = document.getElementById('password');
- let login = document.getElementById('login');
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+let login = document.getElementById('login');
+let Form = document.querySelector('form');
 
 
+ login.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    let ValidEmail = "admin@gmail.com";
+    let ValidPassword = "admin1234";
  
+    if(email.value !== ValidEmail || password.value!== ValidPassword){
+        alert('please Enter a valid password or email');
+        return;
+    }
+        window.location.href = "Managment.html";
+      
+});
