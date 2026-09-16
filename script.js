9b9e606c0ef5bc85b2f5bef3ae895241e6c15797
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-09-16 22:32:19 +0500

    Employee Managment System

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ const themeToggle = document.querySelector("#themeToggle");
 const menuToggle = document.querySelector("#menuToggle");
 const navLinks = document.querySelector(".nav-links");
 
-
 /* SAVED THEME */
-
 const savedTheme = localStorage.getItem("theme");
 
 if (savedTheme === "dark") {
@@ -15,33 +13,21 @@ if (savedTheme === "dark") {
     themeToggle.innerHTML = '<i class="fas fa-moon"></i>';
 }
 
-
 /* THEME TOGGLE */
-
 themeToggle.addEventListener("click", function () {
-
     document.body.classList.toggle("dark-mode");
 
     if (document.body.classList.contains("dark-mode")) {
-
         localStorage.setItem("theme", "dark");
         themeToggle.innerHTML = '<i class="fas fa-sun"></i>';
-
     } else {
-
         localStorage.setItem("theme", "light");
         themeToggle.innerHTML = '<i class="fas fa-moon"></i>';
-
     }
-
 });
 
-
 /* MOBILE MENU */
-
 menuToggle.addEventListener("click", function () {
-
     navLinks.classList.toggle("show");
-
-});themeToggle.innerHTML = '<i class="fas fa-sun"></i>';
+});
 
